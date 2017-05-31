9aa51fc2ffe78b966c7f4fc4302b3dc2b80d9fc1
Tim McLuckie <tim.mcluckie@gmail.com>
2017-05-30 20:20:06 -0400

    Added missing files from previous commit

---
+++ src/main/java/com/actualize/mortgage/mortgagemodel/CashFlowInfo.java
@@ -0,0 +1,23 @@
+package com.actualize.mortgage.mortgagemodel;
+
+public enum CashFlowInfo {
+	BALANCE("%9.2f"),
+	PRINCIPAL_AND_INTEREST_PAYMENT("%9.2f"),
+	TOTAL_PAYMENT("%9.2f"),
+	INTEREST_RATE("%3.5f"),
+	PRINCIPAL_PAYMENT("%9.2f", PRINCIPAL_AND_INTEREST_PAYMENT, TOTAL_PAYMENT),
+	INTEREST_PAYMENT("%9.2f", PRINCIPAL_AND_INTEREST_PAYMENT, TOTAL_PAYMENT),
+	MORTGAGE_INSURANCE_PAYMENT("%9.2f", TOTAL_PAYMENT);
+
+	public final String printFormat;
+	public final CashFlowInfo[] sumTo;
+	
+	private CashFlowInfo(String printFormat, CashFlowInfo... sumTo) {
+		this.printFormat = printFormat;
+		this.sumTo = new CashFlowInfo[sumTo.length];
+		int i = 0;
+		for (CashFlowInfo s : sumTo)
+			this.sumTo[i++] = s;
+	}
+	
+}
---
+++ src/main/java/com/actualize/mortgage/mortgagemodel/CashFlowResult.java
@@ -0,0 +1,53 @@
+package com.actualize.mortgage.mortgagemodel;
+
+import java.util.TreeMap;
+
+import com.actualize.mortgage.ucd.calculationutils.Functions;
+
+public class CashFlowResult {
+	private final Object[] data;
+	public final int length;
+
+	public CashFlowResult(int periods) {
+		this.data = new Object[periods];
+		this.length = periods;
+	}
+
+	public double apr(double upfrontCosts) {
+		double[] cf = new double[length + 1];
+		cf[0] = -getValue(0, CashFlowInfo.BALANCE) + upfrontCosts;
+		for (int i = 1; i < cf.length; i++)
+			cf[i] = getValue(0, CashFlowInfo.TOTAL_PAYMENT);
+		return 12.0 * Functions.irr(cf);
+	}
+
+	public CashFlowResult addValue(int month, CashFlowInfo info, double value) {
+		addBase(month, info, value);
+		for (CashFlowInfo cfi : info.sumTo)
+			addBase(month, cfi, value);
+		return this;
+	}
+	
+	public double getValue(int month, CashFlowInfo... info) {
+		if (data[month] == null) {
+			data[month] = new TreeMap<CashFlowInfo, Double>();
+			return 0;
+		}
+		double accum = 0;
+		@SuppressWarnings("unchecked")
+		TreeMap<CashFlowInfo, Double> periodData = ((TreeMap<CashFlowInfo, Double>)data[month]);
+		for (CashFlowInfo cfi : info) {
+			Double value = periodData.get(cfi);
+			if (value != null)
+				accum += value;
+		}
+		return accum;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void addBase(int month, CashFlowInfo info, double value) {
+		value += getValue(month, info);
+		((TreeMap<CashFlowInfo, Double>)data[month]).put(info, Functions.round(value));
+	}
+	
+}
